61db69cb2924a08ad7e5f0a0afdb4ca10e236e55
RicardoACS <carrasco10@gmail.com>
2015-07-27 22:47:42 -0300

    update y delete en carrito, iconos en carrito

--- carrito.php
+++ carrito.php
@@ -35,16 +35,48 @@ session_start();
         }       
         if (isset($_SESSION['carrito'])) 
         {
+            //Sirva para comprar y que no se borre
             $carrito = $_SESSION['carrito'];
             if(isset($_POST['txt_id']))
             {                          
                 $id        =  $_POST['txt_id'];
                 $nombre    =  $_POST['txt_nombre'];
                 $cantidad  =  $_POST['txt_cantidad'];       
-                $precio    =  $_POST['txt_precio'];  
-                $carrito[] = array('id' => $id,'nombre' => $nombre, 
+                $precio    =  $_POST['txt_precio'];
+                $pos       = -1;
+                for ($i = 0; $i<count($carrito);$i++)
+                {
+                    if ($id == $carrito[$i]['id'])
+                    {
+                        $pos = $i;
+                    }
+                }
+                if ($pos != -1) 
+                {
+                   $cuanto = $carrito[$pos]['cantidad'] + $cantidad; 
+                   $carrito[$pos] = array('id' => $id,'nombre' => $nombre, 
+                                'precio' => $precio,'cantidad' => $cuanto); 
+                }
+                else
+                {
+                   $carrito[] = array('id' => $id,'nombre' => $nombre, 
                                 'precio' => $precio,'cantidad' => $cantidad);
+                }
+               
              }
+        }
+        //Actualización de cantida de producto en carrito
+        if (isset($_POST['indice2'])) 
+        {
+            $indice              = $_POST['indice2'];
+            $cantidadActualizada = $_POST['cant2'];
+            $carrito[$indice]['cantidad'] = $cantidadActualizada;
+        }
+        //Delete de un producto en carrito
+        if (isset($_POST['indice3'])) 
+        {
+            $indice              = $_POST['indice3'];
+            $carrito[$indice]    = NULL;
         }
         if(isset($carrito))
         {
@@ -69,7 +101,9 @@ session_start();
                    {
                        
                        for($i = 0; $i < count($carrito);$i++)
-                        {                          
+                        { 
+                           if ($carrito[$i] != NULL) 
+                               {              
                 ?>
                 <tr>
                     <td>
@@ -79,17 +113,33 @@ session_start();
                         <?php echo $carrito[$i]['precio']?>
                     </td>
                     <td>
-                        <?php echo $carrito[$i]['cantidad']?>
+                        <form name="update" action="carrito.php" method="POST">     
+                            <!--Este campo hidden permitirá saber que producto será actualizado -->
+                            <input type="hidden" name="indice2" value="<?php echo $i ?>" />
+                            <input type="text" name="cant2" 
+                                   value="<?php echo $carrito[$i]['cantidad']?>" maxlength="2" size="2"/>
+                            <input type="image" name = "btn_update" src="Iconos/update.jpg" 
+                                   width="20" height="20" alt="update"/>
+                        </form>
                     </td>
                     <td>
                         <?php
                         $subTotal = $carrito[$i]['precio'] * $carrito[$i]['cantidad'];
                         $total += $subTotal;
                         ?>
-                        <?php echo $subTotal; ?>
+                        <?php echo $subTotal; ?>                       
+                    </td>
+                    <td>
+                        <form method="POST">
+                            <input type="hidden" name="indice3" value="<?php echo $i ?>" />
+                            <input type="image" src="Iconos/delete.jpg" 
+                                   width="20" height="20" alt="delete"/>
+
+                        </form>
                     </td>
                 </tr>
                 <?php
+                            }
                         }
                     }
                 ?>
